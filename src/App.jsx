0e6d1a1725b2029128a5e4bb0c38079163bf27e2
patel <neelp060@gmail.com>
2026-02-09 21:33:17 -0500

    SeachBar functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import './App.css'
+import SearchBar from "./Components/SearchBar"
 
 function App() {
-  
+
+  function getUser (user){
+    console.log(user);
+  }
+
+
   return (
-    
-      <div>
-        <h1 className="text-3xl font-bold underline"> "Hello world!" </h1>
+    <div className="min-h-screen flex justify-center items-center bg-gray-100">     
+      <div className="w-full max-w-3xl p-6 bg-white rounded-lg shadow">       
+        <h1 className="text-3xl font-bold mb-4 text-center">
+          AI Resume Analyzer
+        </h1>
+        <main>
+          <SearchBar GetUser={getUser}/>
+        </main>
       </div>
-     
+    </div>
   )
 }
 
